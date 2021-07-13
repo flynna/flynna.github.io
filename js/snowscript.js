02d03e4ba724ba7826a173cd132f42bcc9309714
zhanghelin <zhanghelin@sobey.com>
2021-07-13 20:15:26 +0800

    Site updated: 2021-07-13 20:15:25

--- js/snowscript.js
+++ js/snowscript.js
@@ -5,7 +5,7 @@ var snowConfig = {
   color: [105 / 256, 165 / 256, 239 / 256],
   speed: 2e-7, // 速度
   gravity: 70000, // 重力
-  count: 80,
+  count: 200,
   opacity: 0.3,
-  density: 1 / 80, // 密度
+  density: 1 / 50, // 密度
 };
