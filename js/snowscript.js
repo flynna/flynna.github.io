8c9f5e5a2fb4db9d3b3791ec572f5ef0e82b040e
zhanghelin <zhanghelin@sobey.com>
2021-07-13 17:05:20 +0800

    Site updated: 2021-07-13 17:05:17

---
+++ js/snowscript.js
@@ -0,0 +1,11 @@
+// See sources on GitHub:
+// https://github.com/mkalygin/snowflakes
+
+var snowConfig = {
+  color: [105 / 256, 165 / 256, 239 / 256],
+  speed: 2e-7, // 速度
+  gravity: 70000, // 重力
+  count: 80,
+  opacity: 0.3,
+  density: 1 / 80, // 密度
+};
